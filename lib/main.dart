a67a8b01be0799e9f2014cf285ee263928c6e4cb
Katarzyna Podkowa <kasia-podkowa@wp.pl>
2019-11-15 16:01:33 +0100

    zaj2

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart' as prefix0;
+import 'package:silverflutter/my_card.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<bool> stars = [false,false,false,false,false,false];
+
+  void toggleStar (int index){
+    setState(() {
+      stars[index] = !stars[index];
+    });
+  }
+
+  void setAllStarred(){
+    setState(() {
+      for (var i = 0; i < stars.length; i++) {
+        stars[i] = true;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<String> titles = ["title1","title2","title3","title4","title5","title6"];
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Pierwsze zajęcia!"),
-          ),
-          body: Column(
-            children: <Widget>[
-              Padding(
-                  padding: EdgeInsets.all(5),
-                  child: Card(
-                      color: Colors.grey,
-                      child: Column(children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Text("Opportunity"),
-                            FlatButton.icon(
-                              onPressed: () {},
-                              icon: Icon(Icons.star),
-                              label: Text("star it"),
-                            )
-                          ],
-                        ),
-                        Image.asset("assets/opportunity.jpg"),
-                      ]
-                      )
-                      )
-                      )
-            ],
-          )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: this.setAllStarred,
+          child: Icon(Icons.star),
+        ),
+        appBar: AppBar(
+          title: Text("Pierwsze zajęcia!"),
+        ),
+        body: ListView(
+          children: titles.asMap().map((int index,String title){
+            return MapEntry(
+              index, MyCard.titleOnly(
+                title: title, 
+                onStarIt: ()=>this.toggleStar(index), 
+                isStarred: stars[index]));
+          }).values.toList(),
+        ),
+      ),
     );
   }
 }
